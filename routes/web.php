87bede01d414d2ea88812f59c856d35d98160a08
IKY <541231057@student.smktelkom-pwt.sch.id>
2024-10-14 15:32:25 +0700

    Rifqi

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/welcome', function () {
+    return view('welcome'); // ke halaman welcome.blade.php
+});
+
+Route::get('/index', function () {
+    return view('index'); // ke halaman index.blade.php
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('login'); // ke halaman login.blade.php
 });
